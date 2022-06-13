d67955f386d779fbad3de2bdd72557b6d9764fa7
Nimra Nasir <nimra.nasir@norgic.com>
2022-06-13 15:22:13 +0500

    Open meet.vdotok and send vdotoklinks

--- task.py
+++ task.py
@@ -23,6 +23,8 @@ for i in range(10):
     driver.maximize_window()
     driver.get('https://meet.vdotok.com/?t=1654156110537')
     driver.implicitly_wait(5)
+    //  hhjhj
+
 
     name = driver.find_element(By.XPATH, "//input[@id='username']")
     name.send_keys("Nimra")
